1b7ba8518c30764232e5875930a1e0b918d0a506
Aleksey Kurdyukov <alkurdy@gmail.com>
2022-01-29 22:50:12 -0500

    New commit

---
+++ Main.cs
@@ -0,0 +1,11 @@
+using System;
+using actors;
+class Program
+{
+           static void Main(string[] args)
+        {
+            /*actor newObj = new actor(10,100,"Kurdyukov","Aleksey",DateTime.Now);*/
+            DateTime bday = new DateTime(1988, 09, 05);
+            Console.WriteLine(DateTime.Now.Year);
+        }
+}
---
+++ actors_class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace actors
+{
+    class actor
+    {
+        private string fName;
+        private string lName;
+        private DateTime birthDate;
+        private int age;
+        private int health;
+        private int strength;
+        private int hunger;
+        private int thirst;
+        private int bodytemp;
+        private int energy;
+        private int fatique;
+
+
+
+        public actor(int strength, int health, string lName, string fName, DateTime birthDate = default, int age = 0)
+        {
+            this.strength = strength;
+            this.health = health;
+            this.lName = lName;
+            this.FName = fName;
+            this.birthDate = birthDate;
+            this.age = age;
+        }
+
+        public string FName { get => fName; set => fName = value; }
+        public DateTime BirthDate { get => birthDate; set => birthDate = value; }
+    }
+}
